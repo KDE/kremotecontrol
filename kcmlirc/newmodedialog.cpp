da508b4190e0ff70eef95631bc384b3d1e254051
David Faure <faure@kde.org>
2006-04-04 21:43:35 +0000

    Now with license header
    svn path=/trunk/KDE/kdeutils/kdelirc/; revision=526546

--- kcmlirc/newmodedialog.cpp
+++ kcmlirc/newmodedialog.cpp
@@ -1,3 +1,13 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Gav Wood                                        *
+ *   gav@kde.org                                                           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
 #include "newmodedialog.h"
 
 NewModeDialog::NewModeDialog( QWidget* parent )
@@ -5,8 +15,8 @@ NewModeDialog::NewModeDialog( QWidget* parent )
 {
     setupUi(this);
 
-    theRemotes.addColumn( i18n( "Remote Controls" ) );
-    theRemotes.setFullWidth( true );
+    theRemotes->addColumn( i18n( "Remote Controls" ) );
+    theRemotes->setFullWidth( true );
 
     // signals and slots connections
     connect(kPushButton6, SIGNAL(clicked()), this, SLOT(accept()));
--- kcmlirc/newmodedialog.h
+++ kcmlirc/newmodedialog.h
@@ -1,3 +1,13 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Gav Wood                                        *
+ *   gav@kde.org                                                           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
 #ifndef NEWMODEDIALOG_H
 #define NEWMODEDIALOG_H
 
