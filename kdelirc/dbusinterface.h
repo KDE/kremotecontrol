f459c83142ee58445472ae8a8f41f2f6c83c4ff0
Michael Zanetti <mzanetti@kde.org>
2009-03-08 19:39:10 +0000

    added isProgramRunning() to DBusInterface
    svn path=/trunk/playground/utils/kdelirc/; revision=936983

--- kdelirc/dbusinterface.h
+++ kdelirc/dbusinterface.h
@@ -1,8 +1,27 @@
+/*************************************************************************
+ * Copyright            : (C) 2003 by Gav Wood <gav@kde.org>             *
+ *                                                                       *
+ * This program is free software; you can redistribute it and/or         *
+ * modify it under the terms of the GNU General Public License as        *
+ * published by the Free Software Foundation; either version 2 of        *
+ * the License or (at your option) version 3 or any later version        *
+ * accepted by the membership of KDE e.V. (or its successor approved     *
+ * by the membership of KDE e.V.), which shall act as a proxy            *
+ * defined in Section 14 of version 3 of the license.                    *
+ *                                                                       *
+ * This program is distributed in the hope that it will be useful,       *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *************************************************************************/
 /*
- * dbusqueryinterface.h
+ * dbusinterface.h
  *
  *  Created on: 14.02.2009
- *      Author: frank
+ *      Author: Michael Zanetti
  */
 
 #ifndef DBUSINTERFACE_H
@@ -12,40 +31,6 @@
 #include "iraction.h"
 
 #include <QStringList>
-
-#include <ktoolinvocation.h>
-
-
-class DBusProgramm
-{
-private:
-  QString theName;
-  QString theFormattedName;
-  bool theMultiInstance;
-
-public:
-
-  DBusProgramm(){};
-  DBusProgramm(QString name, QString formattedName, bool multiIntance)
-  {
-    theName = name;
-    theFormattedName = formattedName;
-    theMultiInstance = multiIntance;
-  }
-  const QString name()
-  {
-    return theName;
-  };
-  const QString formattedName()
-  {
-    return theFormattedName;
-  };
-   bool multiInstance()
-  {
-    return theMultiInstance;
-  };
-
-};
 
 class DBusInterface
 {
@@ -61,6 +46,8 @@ public:
   ~DBusInterface();
 
   static DBusInterface *getInstance();
+
+  bool isProgramRunning(const QString &program);
 
   QStringList getRegisteredPrograms();
   QStringList getObjects(const QString &program);
