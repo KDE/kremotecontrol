a6c0ad4ba7f151813609fa6341951b191803d653
Michael Zanetti <mzanetti@kde.org>
2010-03-05 19:15:13 +0000

    fix copyright headers (Krazy)
    svn path=/branches/work/kdelirc/; revision=1099571

--- kcmremotecontrol/modedialog.cpp
+++ kcmremotecontrol/modedialog.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright: (C) 2010 by Michael Zanetti <michael_zanetti@gmx.net>      *
+ * Copyright (C) 2010 by Michael Zanetti <michael_zanetti@gmx.net>       *
  *                                                                       *
  * This program is free software; you can redistribute it and/or         *
  * modify it under the terms of the GNU General Public License as        *
--- kcmremotecontrol/model.cpp
+++ kcmremotecontrol/model.cpp
@@ -1,6 +1,6 @@
 /*************************************************************************
-* Copyright: (C) 2009 by Frank Scheffold <fscheffold@googlemail.com>    *
-* Copyright: (C) 2009 by Michael Zanetti <michael_zanetti@gmx.net>      *
+* Copyright (C) 2009 by Frank Scheffold <fscheffold@googlemail.com>     *
+* Copyright (C) 2009 by Michael Zanetti <michael_zanetti@gmx.net>       *
 *                                                                       *
 * This program is free software; you can redistribute it and/or         *
 * modify it under the terms of the GNU General Public License as        *
